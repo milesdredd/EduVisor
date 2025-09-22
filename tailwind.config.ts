de12129f02a472260c1e984f020e1b9c2706769a
Raj 08 <ankitraj97093@gmail.com>
2025-09-22 13:53:23 +0000

    ouh but i can see the currency text used is showing different , can we u

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
-        code: ['monospace'],
+        body: ['"JetBrains Mono"', 'sans-serif'],
+        headline: ['"JetBrains Mono"', 'sans-serif'],
+        code: ['"JetBrains Mono"', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
